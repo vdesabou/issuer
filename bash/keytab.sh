2e21cecd9d6e6c0c0515e06f55ebba8a5d4d58aa
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-07-19 16:19:44 +0200

    to avoid KrbException: Message stream modified (41)

--- bash/keytab.sh
+++ bash/keytab.sh
@@ -3,4 +3,5 @@ keytab(){
   echo "Generating keytab"
   kadmin -kt /tmp/admin.keytab -p admin/admin -q "addprinc -randkey $1"
   kadmin -kt /tmp/admin.keytab -p admin/admin -q "ktadd -k $2 $1"
+  kadmin -kt /tmp/admin.keytab -p admin/admin -q "modprinc -maxlife 11days -maxrenewlife 11days +allow_renewable $1"
 }
