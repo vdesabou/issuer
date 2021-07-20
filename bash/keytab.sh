0f76299162ebd9ee209129b96783a877fcb5209f
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-07-20 09:50:31 +0200

    update

--- bash/keytab.sh
+++ bash/keytab.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 keytab(){
   echo "Generating keytab"
+  kadmin -kt /tmp/admin.keytab -p admin/admin -q "modprinc -maxrenewlife 11days +allow_renewable krbtgt/EXAMPLE.COM"
   kadmin -kt /tmp/admin.keytab -p admin/admin -q "addprinc -randkey $1"
-  kadmin -kt /tmp/admin.keytab -p admin/admin -q "ktadd -k $2 $1"
   kadmin -kt /tmp/admin.keytab -p admin/admin -q "modprinc -maxlife 11days -maxrenewlife 11days +allow_renewable $1"
+  kadmin -kt /tmp/admin.keytab -p admin/admin -q "ktadd -k $2 $1"
 }
